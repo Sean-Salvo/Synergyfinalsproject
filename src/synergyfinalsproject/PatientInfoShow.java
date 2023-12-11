308ba4dad46d257a63fdeb44d09e3b5a0f445946
Oweneega <150930671+Oweneega@users.noreply.github.com>
2023-12-11 22:25:28 +0800

    a

--- src/synergyfinalsproject/PatientInfoShow.java
+++ src/synergyfinalsproject/PatientInfoShow.java
@@ -1,30 +1,23 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package synergyfinalsproject;
 
-
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
-import javax.swing.SwingUtilities;
 
 /**
  *
  * @author Test
  */
-public class HomeAdmin extends javax.swing.JFrame {
+public class PatientInfoShow extends javax.swing.JFrame {
 
     /**
-     * Creates new form HomeAdmin
+     * Creates new form PatientInfoShow
      */
-    public HomeAdmin() {
+    public PatientInfoShow() {
         initComponents();
-        
-        
     }
 
     /**
@@ -36,8 +29,6 @@ public class HomeAdmin extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
         jPanel1 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
@@ -56,6 +47,35 @@ public class HomeAdmin extends javax.swing.JFrame {
         jLabel11 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         jPanel4 = new javax.swing.JPanel();
+        jPanel10 = new javax.swing.JPanel();
+        jLabel14 = new javax.swing.JLabel();
+        jSeparator2 = new javax.swing.JSeparator();
+        jSeparator3 = new javax.swing.JSeparator();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel15 = new javax.swing.JLabel();
+        jLabel16 = new javax.swing.JLabel();
+        jLabel17 = new javax.swing.JLabel();
+        jLabel18 = new javax.swing.JLabel();
+        jLabel19 = new javax.swing.JLabel();
+        jLabel20 = new javax.swing.JLabel();
+        jLabel21 = new javax.swing.JLabel();
+        jLabel22 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTextArea2 = new javax.swing.JTextArea();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTextArea3 = new javax.swing.JTextArea();
+        jScrollPane4 = new javax.swing.JScrollPane();
+        jTextArea4 = new javax.swing.JTextArea();
+        jScrollPane5 = new javax.swing.JScrollPane();
+        jTextArea5 = new javax.swing.JTextArea();
+        jScrollPane7 = new javax.swing.JScrollPane();
+        jTextArea7 = new javax.swing.JTextArea();
+        jScrollPane8 = new javax.swing.JScrollPane();
+        jTextArea8 = new javax.swing.JTextArea();
+        jScrollPane9 = new javax.swing.JScrollPane();
+        jTextArea9 = new javax.swing.JTextArea();
+        jScrollPane10 = new javax.swing.JScrollPane();
+        jTextArea10 = new javax.swing.JTextArea();
         jScrollPane6 = new javax.swing.JScrollPane();
         jTextArea6 = new javax.swing.JTextArea();
         jPanel8 = new javax.swing.JPanel();
@@ -69,10 +89,6 @@ public class HomeAdmin extends javax.swing.JFrame {
         jPanel18 = new javax.swing.JPanel();
         jPanel19 = new javax.swing.JPanel();
         jCalendar1 = new com.toedter.calendar.JCalendar();
-
-        jTextArea1.setColumns(20);
-        jTextArea1.setRows(5);
-        jScrollPane1.setViewportView(jTextArea1);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -185,8 +201,8 @@ public class HomeAdmin extends javax.swing.JFrame {
         javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
         jPanel5.setLayout(jPanel5Layout);
         jPanel5Layout.setHorizontalGroup(
-            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel5Layout.createSequentialGroup()
+            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel5Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel11, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -216,6 +232,168 @@ public class HomeAdmin extends javax.swing.JFrame {
         jPanel4.setBackground(new java.awt.Color(255, 255, 255));
         jPanel4.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 204, 0), 3, true));
         jPanel4.setLayout(null);
+
+        jPanel10.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel10.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 51, 204), 3, true));
+        jPanel10.setLayout(null);
+
+        jLabel14.setBackground(new java.awt.Color(0, 102, 255));
+        jLabel14.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        jLabel14.setText("Health Conditions - ");
+        jPanel10.add(jLabel14);
+        jLabel14.setBounds(20, 250, 120, 20);
+        jPanel10.add(jSeparator2);
+        jSeparator2.setBounds(0, 50, 270, 0);
+        jPanel10.add(jSeparator3);
+        jSeparator3.setBounds(0, 50, 270, 0);
+
+        jLabel4.setBackground(new java.awt.Color(0, 102, 255));
+        jLabel4.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        jLabel4.setText("Name:");
+        jPanel10.add(jLabel4);
+        jLabel4.setBounds(460, 180, 50, 10);
+
+        jLabel15.setBackground(new java.awt.Color(0, 102, 255));
+        jLabel15.setFont(new java.awt.Font("Times New Roman", 0, 24)); // NOI18N
+        jLabel15.setText("Patient Information");
+        jPanel10.add(jLabel15);
+        jLabel15.setBounds(40, 10, 190, 28);
+
+        jLabel16.setBackground(new java.awt.Color(0, 102, 255));
+        jLabel16.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        jLabel16.setText("Room Number - ");
+        jPanel10.add(jLabel16);
+        jLabel16.setBounds(20, 40, 100, 20);
+
+        jLabel17.setBackground(new java.awt.Color(0, 102, 255));
+        jLabel17.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        jLabel17.setText("Patient Name -");
+        jPanel10.add(jLabel17);
+        jLabel17.setBounds(20, 70, 100, 20);
+
+        jLabel18.setBackground(new java.awt.Color(0, 102, 255));
+        jLabel18.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        jLabel18.setText("Patient ID - ");
+        jPanel10.add(jLabel18);
+        jLabel18.setBounds(20, 100, 100, 20);
+
+        jLabel19.setBackground(new java.awt.Color(0, 102, 255));
+        jLabel19.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        jLabel19.setText("Admitted Date - ");
+        jPanel10.add(jLabel19);
+        jLabel19.setBounds(20, 130, 100, 20);
+
+        jLabel20.setBackground(new java.awt.Color(0, 102, 255));
+        jLabel20.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        jLabel20.setText("Discharge Date -");
+        jPanel10.add(jLabel20);
+        jLabel20.setBounds(20, 160, 100, 20);
+
+        jLabel21.setBackground(new java.awt.Color(0, 102, 255));
+        jLabel21.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        jLabel21.setText("Assigned Doctor - ");
+        jPanel10.add(jLabel21);
+        jLabel21.setBounds(20, 190, 130, 10);
+
+        jLabel22.setBackground(new java.awt.Color(0, 102, 255));
+        jLabel22.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        jLabel22.setText("Notes - ");
+        jPanel10.add(jLabel22);
+        jLabel22.setBounds(20, 220, 110, 17);
+
+        jScrollPane2.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        jScrollPane2.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+
+        jTextArea2.setColumns(20);
+        jTextArea2.setTabSize(0);
+        jTextArea2.setText("test\ntest");
+        jTextArea2.setEnabled(false);
+        jScrollPane2.setViewportView(jTextArea2);
+
+        jPanel10.add(jScrollPane2);
+        jScrollPane2.setBounds(130, 70, 130, 20);
+
+        jScrollPane3.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        jScrollPane3.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+
+        jTextArea3.setColumns(20);
+        jTextArea3.setTabSize(0);
+        jTextArea3.setEnabled(false);
+        jScrollPane3.setViewportView(jTextArea3);
+
+        jPanel10.add(jScrollPane3);
+        jScrollPane3.setBounds(130, 250, 130, 20);
+
+        jScrollPane4.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        jScrollPane4.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+
+        jTextArea4.setColumns(20);
+        jTextArea4.setTabSize(0);
+        jTextArea4.setText("test");
+        jTextArea4.setEnabled(false);
+        jScrollPane4.setViewportView(jTextArea4);
+
+        jPanel10.add(jScrollPane4);
+        jScrollPane4.setBounds(130, 40, 130, 20);
+
+        jScrollPane5.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        jScrollPane5.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+
+        jTextArea5.setColumns(20);
+        jTextArea5.setTabSize(0);
+        jTextArea5.setEnabled(false);
+        jScrollPane5.setViewportView(jTextArea5);
+
+        jPanel10.add(jScrollPane5);
+        jScrollPane5.setBounds(130, 100, 130, 20);
+
+        jScrollPane7.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        jScrollPane7.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+
+        jTextArea7.setColumns(20);
+        jTextArea7.setTabSize(0);
+        jTextArea7.setEnabled(false);
+        jScrollPane7.setViewportView(jTextArea7);
+
+        jPanel10.add(jScrollPane7);
+        jScrollPane7.setBounds(130, 130, 130, 20);
+
+        jScrollPane8.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        jScrollPane8.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+
+        jTextArea8.setColumns(20);
+        jTextArea8.setTabSize(0);
+        jTextArea8.setEnabled(false);
+        jScrollPane8.setViewportView(jTextArea8);
+
+        jPanel10.add(jScrollPane8);
+        jScrollPane8.setBounds(130, 160, 130, 20);
+
+        jScrollPane9.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        jScrollPane9.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+
+        jTextArea9.setColumns(20);
+        jTextArea9.setTabSize(0);
+        jTextArea9.setEnabled(false);
+        jScrollPane9.setViewportView(jTextArea9);
+
+        jPanel10.add(jScrollPane9);
+        jScrollPane9.setBounds(130, 190, 130, 20);
+
+        jScrollPane10.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        jScrollPane10.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+
+        jTextArea10.setColumns(20);
+        jTextArea10.setTabSize(0);
+        jTextArea10.setEnabled(false);
+        jScrollPane10.setViewportView(jTextArea10);
+
+        jPanel10.add(jScrollPane10);
+        jScrollPane10.setBounds(130, 220, 130, 20);
+
+        jPanel4.add(jPanel10);
+        jPanel10.setBounds(10, 10, 270, 290);
+
         jPanel3.add(jPanel4);
         jPanel4.setBounds(160, 50, 290, 310);
 
@@ -320,58 +498,58 @@ public class HomeAdmin extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void BackButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BackButtonActionPerformed
+
+        JFrame back = new HomeAdmin();
+        if (back != null) {
+            back.dispose();
+        }
+
+        // Close the current frame
+        this.dispose();
+    }//GEN-LAST:event_BackButtonActionPerformed
+
     private void HomeButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_HomeButtonActionPerformed
         JFrame home = new HomeAdmin();
         home.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         home.setVisible(true);
-        
-          if (this.isVisible()) {
-        this.dispose();
-    }
+
+        if (this.isVisible()) {
+            this.dispose();
+        }
     }//GEN-LAST:event_HomeButtonActionPerformed
+
+    private void DoorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_DoorActionPerformed
+        JFrame settings = new RoomAdminListview();
+        settings.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        settings.setVisible(true);
+        if (this.isVisible()) {
+            this.dispose();
+        }
+    }//GEN-LAST:event_DoorActionPerformed
 
     private void SettingsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SettingsActionPerformed
         JFrame settingsFrame = new SettingsNgAdmin();
         settingsFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         settingsFrame.setVisible(true);
-        
-          if (this.isVisible()) {
-        this.dispose();
-    }
+
+        if (this.isVisible()) {
+            this.dispose();
+        }
     }//GEN-LAST:event_SettingsActionPerformed
 
     private void InfoButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_InfoButtonActionPerformed
         JFrame info = new Synergy_InfoAboutUS();
         info.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         info.setVisible(true);
-          if (this.isVisible()) {
-        this.dispose();
-    }
-        
+        if (this.isVisible()) {
+            this.dispose();
+        }
+
     }//GEN-LAST:event_InfoButtonActionPerformed
 
-    private void BackButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BackButtonActionPerformed
-       
-        JFrame back = new HomeAdmin();
-                if (back != null) {
-        back.dispose();
-    }
-
-    // Close the current frame
-    this.dispose();
-    }//GEN-LAST:event_BackButtonActionPerformed
-
-    private void DoorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_DoorActionPerformed
-        JFrame settings = new RoomAdminListview();
-        settings.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        settings.setVisible(true);
-            if (this.isVisible()) {
-        this.dispose();
-    }
-    }//GEN-LAST:event_DoorActionPerformed
-
     private void jList3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jList3MouseClicked
-           
+
         if(jList3.getSelectedIndex() == 0) {
             JFrame RoomInfo = new PatientInfoShow();
             
@@ -401,20 +579,20 @@ public class HomeAdmin extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(HomeAdmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PatientInfoShow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(HomeAdmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PatientInfoShow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(HomeAdmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PatientInfoShow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(HomeAdmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PatientInfoShow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new HomeAdmin().setVisible(true);
+                new PatientInfoShow().setVisible(true);
             }
         });
     }
@@ -427,12 +605,23 @@ public class HomeAdmin extends javax.swing.JFrame {
     private javax.swing.JButton Settings;
     private com.toedter.calendar.JCalendar jCalendar1;
     private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel14;
+    private javax.swing.JLabel jLabel15;
+    private javax.swing.JLabel jLabel16;
+    private javax.swing.JLabel jLabel17;
+    private javax.swing.JLabel jLabel18;
+    private javax.swing.JLabel jLabel19;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel20;
+    private javax.swing.JLabel jLabel21;
+    private javax.swing.JLabel jLabel22;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel9;
     private javax.swing.JList<String> jList3;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel10;
     private javax.swing.JPanel jPanel11;
     private javax.swing.JPanel jPanel12;
     private javax.swing.JPanel jPanel13;
@@ -448,10 +637,26 @@ public class HomeAdmin extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel5;
     private javax.swing.JPanel jPanel6;
     private javax.swing.JPanel jPanel8;
-    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane10;
     private javax.swing.JScrollPane jScrollPane17;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JScrollPane jScrollPane4;
+    private javax.swing.JScrollPane jScrollPane5;
     private javax.swing.JScrollPane jScrollPane6;
-    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JScrollPane jScrollPane7;
+    private javax.swing.JScrollPane jScrollPane8;
+    private javax.swing.JScrollPane jScrollPane9;
+    private javax.swing.JSeparator jSeparator2;
+    private javax.swing.JSeparator jSeparator3;
+    private javax.swing.JTextArea jTextArea10;
+    private javax.swing.JTextArea jTextArea2;
+    private javax.swing.JTextArea jTextArea3;
+    private javax.swing.JTextArea jTextArea4;
+    private javax.swing.JTextArea jTextArea5;
     private javax.swing.JTextArea jTextArea6;
+    private javax.swing.JTextArea jTextArea7;
+    private javax.swing.JTextArea jTextArea8;
+    private javax.swing.JTextArea jTextArea9;
     // End of variables declaration//GEN-END:variables
 }
