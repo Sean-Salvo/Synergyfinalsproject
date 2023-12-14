255c8f6b7f570eaf8743c2842ca497dd05360ab2
Andrei <143593514+RazTaLady17@users.noreply.github.com>
2023-12-15 02:13:03 +0800

    Created LinkedList

---
+++ src/synergyfinalsproject/DataBase.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package synergyfinalsproject;
+
+import java.util.LinkedList;
+
+
+public class DataBase {
+    LinkedList PatientName = new LinkedList();
+    LinkedList BirthDate = new LinkedList();
+    LinkedList Gender = new LinkedList();
+    LinkedList IdNumber = new LinkedList();
+    
+    
+    
+    
+}
