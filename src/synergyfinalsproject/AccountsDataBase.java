2f4c97e34bf07b87b8242e30f3004bebf1a3cc59
Andrei <143593514+RazTaLady17@users.noreply.github.com>
2023-12-15 03:31:09 +0800

    Create Account DataBase

---
+++ src/synergyfinalsproject/AccountsDataBase.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package synergyfinalsproject;
+
+import java.util.LinkedList;
+
+public class AccountsDataBase {
+
+    private LinkedList<String> FirstName;
+    private LinkedList<String> MiddleName;
+    private LinkedList<String> SurName;
+    private LinkedList<String> Gender;
+    private LinkedList<String> BirthDate;
+    private LinkedList<String> Status;
+    private LinkedList<String> Email;
+    private LinkedList<String> ContactNum;
+    private LinkedList<String> Addresses;
+
+    public AccountsDataBase() {
+        this.FirstName = new LinkedList<>();
+        this.MiddleName = new LinkedList<>();
+        this.SurName = new LinkedList<>();
+        this.Gender = new LinkedList<>();
+        this.BirthDate = new LinkedList<>();
+        this.Status = new LinkedList<>();
+        this.Email = new LinkedList<>();
+        this.ContactNum = new LinkedList<>();
+        this.Addresses = new LinkedList<>();
+
+    }
+
+    public void addData(String firstName, String middleName, String surName, String gender, String birthDate, String status, String email, String contactNum, String addresses) {
+        FirstName.add(firstName);
+        MiddleName.add(middleName);
+        SurName.add(surName);
+        Gender.add(gender);
+        BirthDate.add(birthDate);
+        Status.add(status);
+        Email.add(email);
+        ContactNum.add(contactNum);
+        Addresses.add(addresses);
+
+    }
+
+    public LinkedList<String> getAllFirstName() {
+        return new LinkedList<>(FirstName);
+    }
+
+    public LinkedList<String> getAllMiddleName() {
+        return new LinkedList<>(MiddleName);
+    }
+
+    public LinkedList<String> getAllSurName() {
+        return new LinkedList<>(SurName);
+    }
+
+    public LinkedList<String> getAllGender() {
+        return new LinkedList<>(Gender);
+    }
+
+    public LinkedList<String> getAllBirthDate() {
+        return new LinkedList<>(BirthDate);
+    }
+
+    public LinkedList<String> getAllStatus() {
+        return new LinkedList<>(Status);
+    }
+
+    public LinkedList<String> getAllEmail() {
+        return new LinkedList<>(Email);
+    }
+
+    public LinkedList<String> getAllContactNum() {
+        return new LinkedList<>(ContactNum);
+    }
+
+    public LinkedList<String> getAllAddresses() {
+        return new LinkedList<>(Addresses);
+    }
+
+}
--- src/synergyfinalsproject/DataBase.java
+++ src/synergyfinalsproject/DataBase.java
@@ -8,12 +8,42 @@ import java.util.LinkedList;
 
 
 public class DataBase {
-    LinkedList PatientName = new LinkedList();
-    LinkedList BirthDate = new LinkedList();
-    LinkedList Gender = new LinkedList();
-    LinkedList IdNumber = new LinkedList();
+ 
     
     
+    private LinkedList<String> names;
+    private LinkedList<String> birthday;
+    private LinkedList<String> gender;
+    private LinkedList<String> IdNum;
     
+    public DataBase () { 
+        
+        this.names = new LinkedList<>();
+        this.birthday = new LinkedList<>();
+        this.gender = new LinkedList<>();
+        this.IdNum = new LinkedList<>();
+}
     
+    public void addData(String name, String Birthday, String Gender, String idNum) {
+        names.add(name);
+        birthday.add(Birthday);
+        gender.add(Gender);
+        IdNum.add(idNum);
+    }
+
+     public LinkedList<String> getAllNames() {
+        return new LinkedList<>(names);
+    }
+    
+      public LinkedList<String> getAllBirthday() {
+        return new LinkedList<>(birthday);
+    }
+ 
+        public LinkedList<String> getAllGender() {
+        return new LinkedList<>(gender);
+    }
+    
+          public LinkedList<String> getAllIdNum() {
+        return new LinkedList<>(IdNum);
+    }
 }
